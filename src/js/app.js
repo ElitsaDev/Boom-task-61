02f67ab4b74bb7c2db4276e8fdf223493f407bc4
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-01-22 18:54:29 +0200

    add emoji next to the price

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,9 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
+    // This block will be executed once the page is loaded and ready
+    let hotPrice = document.getElementsByClassName('hot');
+    Array.from(hotPrice).forEach(pr => {
+        pr.textContent = pr.textContent + '🔥'
+    })
 });
